fda8ba1040c668f1328a430c80dda63818ead3b4
Hítalo Silva <hitalos@gmail.com>
2015-07-23 01:52:34 -0300

    Correções para mostrar as fotos do church

--- src/includes/functions.inc.php
+++ src/includes/functions.inc.php
@@ -8,6 +8,7 @@ function queryToJson($res){
 	$ret .= json_encode($res->fetch(), JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE);
 	while($row = $res->fetch()){
 		$ret .= ",\n\t";
+		$row['FotoMembro'] = rawurlencode(utf8_decode($row['FotoMembro']));
 		$ret .= json_encode($row, JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE);
 	}
 	$ret .= "\n]";
